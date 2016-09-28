0f3077fbf05d32fb4de3967fa827e505ddfc9195
Nathan Mitchell <nmitchel@cs.wisc.edu>
2016-09-28 17:14:03 -0500

    Removed SOIL as a dependency from the Assimp plugin as texture loading is now handled by the TextureManager framework.

--- opengl_3d/plugins/Assimp/AssimpRenderable_Model.cpp
+++ opengl_3d/plugins/Assimp/AssimpRenderable_Model.cpp
@@ -11,31 +11,6 @@
 #include <glm/gtx/intersect.hpp>
 
 using namespace Nova;
-
-#include <SOIL/SOIL.h>
-GLint Texture_From_File(const char* path,const std::string& directory)
-{
-    std::string filename(path);
-    filename=directory+'/'+filename;
-    GLuint texture_id;
-    glGenTextures(1,&texture_id);
-    int width,height;
-    unsigned char* image=SOIL_load_image(filename.c_str(),&width,&height,0,SOIL_LOAD_RGB);
-    // assign textures to id
-    glBindTexture(GL_TEXTURE_2D,texture_id);
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    // parameters
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-    glBindTexture(GL_TEXTURE_2D,0);
-    SOIL_free_image_data(image);
-    return texture_id;
-}
-
 //#####################################################################
 // Constructor
 //#####################################################################
